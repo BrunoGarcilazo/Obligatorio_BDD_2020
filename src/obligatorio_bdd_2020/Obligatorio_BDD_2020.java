c3c99258a485e2a1b4899c3f9a24851cf0139c83
Bruno Garcilazo <b.garcilazo@hotmail.com>
2020-10-28 16:27:27 -0300

    Problemas al Registrar

--- src/obligatorio_bdd_2020/Obligatorio_BDD_2020.java
+++ src/obligatorio_bdd_2020/Obligatorio_BDD_2020.java
@@ -18,20 +18,21 @@ import java.sql.Statement;
 public class Obligatorio_BDD_2020{
     
     public static login_utils login = new login_utils();
+
     /**
      * @param args the command line arguments
      * @throws java.sql.SQLException
      */
     public static void main(String[] args) throws SQLException{
         
-        
-        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://192.168.56.102:5432/tests", "postgres", "bruno123")){
-            new main_screen().setVisible(true);
-            System.out.println("Successful connection");
-        }catch (SQLException e){
-
-            System.out.println("Connection failure.");           
-        }                 
+        new main_screen().setVisible(true);
+//        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://192.168.56.102:5432/tests", "postgres", "bruno123")){
+//            
+//            System.out.println("Successful connection");
+//        }catch (SQLException e){
+//
+//            System.out.println("Connection failure.");           
+//        }                 
     }
 
     
--- src/utils/register_utils.java
+++ src/utils/register_utils.java
@@ -24,36 +24,54 @@ import java.util.Locale;
  */
 public class register_utils {
     
-    public static boolean new_registration(String[] data) throws SQLException, ParseException{
+    public static boolean new_registration(String[] data) throws SQLException, ParseException {
         try (Connection connection = DriverManager.getConnection("jdbc:postgresql://192.168.56.102:5432/tests", "postgres", "bruno123")){  
             System.out.println("Successful connection");
-            String query = "SELECT * FROM persona WHERE ci = ?";
-            PreparedStatement statement = connection.prepareStatement(query);
-            statement.setString(1, data[0]);
-            ResultSet rs = statement.executeQuery();           
-            while(rs.next()){
-                return false; // a Person with that ID / C.I already exists.
-            }     
-            String register_query = "INSERT INTO PERSONA VALUES(ci=?,nombre=?,apellido=?,direccion=?,telefono=?,sexo=?,fecha_nac=?);";
+            
+//            String query = "SELECT ci FROM persona";
+//            PreparedStatement statement = connection.prepareStatement(query);
+//            statement.setInt(1,Integer.parseInt(data[0]));
+//            
+//            ResultSet rs = statement.executeQuery();           
+//            while(rs.next()){
+//                //if(rs.getInt(1) == Integer.parseInt(data[0])){
+//                    System.out.println("Ya existe una Persona registrada con ese documento.");               
+//                //}
+//                //return false; // a Person with that ID / C.I already exists.
+//            }     
+            
+            String register_query = "INSERT INTO PERSONA(ci,nombre,apellido,direccion,telefono,sexo,fecha_nac) VALUES(?,?,?,?,?,?,?);";
             PreparedStatement register_statement = connection.prepareStatement(register_query);
             register_statement.setInt(1, Integer.parseInt(data[0]));
             register_statement.setString(2, data[1]);
             register_statement.setString(3, data[2]);
             register_statement.setString(4, data[3]);
             register_statement.setInt(5, Integer.parseInt(data[4]));
-            register_statement.setString(6, data[5]);
-            
+            register_statement.setString(6, data[5]);            
             DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
             Date date = null;
-            try{
+            try {
                 date = (Date) dateFormat.parse(data[6]);
             }catch(ParseException e){
-            }
+            }           
+            register_statement.setDate(7,date); 
             
-            register_statement.setDate(7,date);
+            int resultado = register_statement.executeUpdate();
             
-            ResultSet registerAttempt_rs = register_statement.executeQuery();
-            
+            //register_utils.new_user(data);
+       
+        }         
+        catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Connection failure.");
+            return false;
+        }        
+        return false;
+        
+    }
+    
+    public static boolean new_user(String[] data) throws SQLException{
+        try(Connection connection = DriverManager.getConnection("jdbc:postgresql://192.168.56.102:5432/tests", "postgres", "bruno123")){
             String user_query = "INSERT INTO usuario VALUES(alias=?,ci=?,quienlocreo=?,estado=?,password=?);";
             PreparedStatement user_statement = connection.prepareStatement(user_query);
             user_statement.setString(1,data[7]);
@@ -62,14 +80,11 @@ public class register_utils {
             user_statement.setString(4,"En Espera");
             user_statement.setString(5,BCrypt.hashpw(data[8],BCrypt.gensalt()));
             ResultSet user_query_result = user_statement.executeQuery();
-            return true;
-
-        }         
-        catch (SQLException e) {
-            e.printStackTrace();
+            return true;            
+        }catch (SQLException e) {
             System.out.println("Connection failure.");
             return false;
-        }        
+        }  
         
     }
     
